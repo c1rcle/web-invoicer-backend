8782d7e9a44d8e8ce3d015bafd2ad5e9aca69365
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-11-22 16:57:58 +0100

    Added database migration

---
+++ WebInvoicer.Core/Migrations/20201122155604_FixedUniqueIndex.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebInvoicer.Core.Migrations
+{
+    public partial class FixedUniqueIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Invoices_Number",
+                table: "Invoices");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_Number_UserId",
+                table: "Invoices",
+                columns: new[] { "Number", "UserId" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Invoices_Number_UserId",
+                table: "Invoices");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_Number",
+                table: "Invoices",
+                column: "Number",
+                unique: true);
+        }
+    }
+}
